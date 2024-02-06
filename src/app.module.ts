a2d87cf42467670d5c9ea3256cc967b174ed0412
GiorgiKalatozi <giorgikalatoz@gmail.com>
2024-02-06 13:57:35 +0400

    feat: add graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
