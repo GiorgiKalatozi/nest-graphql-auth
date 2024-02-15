ee4a3b852561ad656a0e9803595e0b4cccfc5a37
GiorgiKalatozi <giorgikalatoz@gmail.com>
2024-02-15 14:55:27 +0400

    feat: create message repository

---
+++ src/modules/messages/messages.repository.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { DeleteUserResponse } from '../users/dtos';
+import { Message } from './entities/message.entity';
+
+@Injectable()
+export class MessagesRepository {
+  constructor(
+    @InjectRepository(Message)
+    private readonly messagesRepository: Repository<Message>,
+  ) {}
+  public async create(message: Partial<Message>): Promise<Message> {
+    return this.messagesRepository.create(message);
+  }
+
+  public async findAll(): Promise<Message[]> {
+    return this.messagesRepository.find();
+  }
+
+  public async findOne(id: string): Promise<Message> {
+    return this.messagesRepository.findOne({ where: { id } });
+  }
+
+  public async update(id: string, message: Message): Promise<Message> {
+    const messageToUpdate = await this.messagesRepository.findOne({
+      where: { id },
+    });
+    await this.messagesRepository.update(id, message);
+    return messageToUpdate;
+  }
+
+  public async save(message: Message): Promise<Message> {
+    return await this.messagesRepository.save(message);
+  }
+
+  public async remove(id: string): Promise<DeleteUserResponse> {
+    await this.messagesRepository.delete({ id });
+
+    return { success: true, message: 'Message removed successfully' };
+  }
+}
