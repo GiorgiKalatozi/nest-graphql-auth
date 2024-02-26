73c942a783326f7c7abecc801351740bdaed65dc
GiorgiKalatozi <giorgikalatoz@gmail.com>
2024-02-26 08:11:13 +0400

    feat: search with elastic in chats

--- src/modules/chats/chats.service.ts
+++ src/modules/chats/chats.service.ts
@@ -1,7 +1,7 @@
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Inject, Injectable } from '@nestjs/common';
 import { Cache } from 'cache-manager';
-import { DataSource, QueryRunner } from 'typeorm';
+import { QueryRunner } from 'typeorm';
 import { PaginationInput } from '../messages/dtos/pagination.input';
 import { Message } from '../messages/entities/message.entity';
 import { SearchService } from '../search/search.service';
@@ -15,7 +15,6 @@ export class ChatsService {
   constructor(
     private readonly chatsRepository: ChatsRepository,
     @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
-    private readonly dataSource: DataSource,
     private readonly searchService: SearchService,
   ) {}
 
@@ -37,10 +36,8 @@ export class ChatsService {
     return this.chatsRepository.save(chat);
   }
   public async searchChats(query: any): Promise<Chat[]> {
-    // Perform Elasticsearch search
     const searchResults = await this.searchService.search('chats', query);
 
-    // Map Elasticsearch search results to Chat entities
     return searchResults.hits.hits.map((hit) => hit._source);
   }
 
--- src/modules/search/search.service.ts
+++ src/modules/search/search.service.ts
@@ -8,39 +8,6 @@ export class SearchService {
     private readonly elasticsearchService: ElasticsearchService,
     private readonly configService: ConfigService,
   ) {}
-  // public async createIndex() {
-  //   const index = this.configService.get('ELASTICSEARCH_INDEX');
-  //   const checkIndex = await this.elasticsearchService.indices.exists({
-  //     index,
-  //   });
-  //   if (checkIndex.statusCode === 404) {
-  //     this.elasticsearchService.indices.create(
-  //       {
-  //         index,
-  //         body: {
-  //           mappings: {
-  //             properties: {
-  //               name: {
-  //                 type: 'text',
-  //                 fields: {
-  //                   keyword: {
-  //                     type: 'keyword',
-  //                     ignore_above: 256,
-  //                   },
-  //                 },
-  //               },
-  //             },
-  //           },
-  //         },
-  //       },
-  //       (err: any) => {
-  //         if (err) {
-  //           console.log(err);
-  //         }
-  //       },
-  //     );
-  //   }
-  // }
 
   public async createIndex(): Promise<void> {
     const index = this.configService.get<string>('ELASTICSEARCH_INDEX');
@@ -84,14 +51,20 @@ export class SearchService {
       body,
     });
   }
-  //   public async indexDocument(data: any) {
-  //     return await this.elasticsearchService.index({
-  //       index: this.configService.get('ELASTICSEARCH_INDEX')!,
-  //       body: data,
-  //     });
-  //   }
 
   public async search(index: string, queryText: string): Promise<any> {
+    return this.elasticsearchService.search({
+      index,
+      body: {
+        query: {
+          wildcard: {
+            'name.keyword': `*${queryText}*`,
+          },
+        },
+      },
+    });
+  }
+  public async searchByWord(index: string, queryText: string): Promise<any> {
     return this.elasticsearchService.search({
       index,
       body: {
@@ -104,21 +77,7 @@ export class SearchService {
       },
     });
   }
-  //   public async search(text: string) {
-  //     const body = await this.elasticsearchService.search<any>({
-  //       index: this.configService.get('ELASTICSEARCH_INDEX')!,
-  //       body: {
-  //         query: {
-  //           multi_match: {
-  //             query: text,
-  //             fields: ['name', 'title', 'username', 'email'],
-  //           },
-  //         },
-  //       },
-  //     });
-  //     const hits = body.hits.hits;
-  //     return hits.map((item) => item._source);
-  //   }
+
   public async remove(id: number, index: string) {
     this.elasticsearchService.deleteByQuery({
       index,
